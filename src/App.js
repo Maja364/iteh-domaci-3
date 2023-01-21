9ac8e822acbd13ecae299c48452d7f1d8307a19e
Maja364 <majaaleksic62@gmail.com>
2023-01-21 19:02:00 +0100

    kreirana komponenta futer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
+ 
 import './App.css';
+import Navbar from './Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
-     
+      <BrowserRouter  >
+          <Navbar ></Navbar>
+          <Routes>
+             
+            
+          </Routes>
+          <Footer></Footer>
+      </BrowserRouter>
     </div>
   );
 }
