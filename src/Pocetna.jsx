cc57fd19f8e0d06e37df35c965cdab1b39886468
Maja364 <majaaleksic62@gmail.com>
2023-01-21 20:01:59 +0100

    omogucena pretraga po nazivu ljubimca

--- src/Pocetna.jsx
+++ src/Pocetna.jsx
@@ -13,13 +13,39 @@ function Pocetna({pregledi}) {
         
         setSort(false);
       }
+
+      function search(){
+        var input, filter, table, tr, td, i, txtValue;
+        input = document.getElementById("myInput");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("myTable");
+        tr = table.getElementsByTagName("tr");
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[2];
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }       
+        }
+    }
+
     return (
       <div className="pocetna"  >
             <div className="buttons">
                 <button className="btn primary" onClick={sortAsc}>Sortiraj rastuće</button>
                 <button className="btn primary" onClick={sortDesc}>Sortiraj opadajuće</button>
+                <input
+                id="myInput"
+                type="text"
+                placeholder="Pretraga po nazivu ljubimca"
+                onChange={search}
+                />
             </div>
-        <table className="table">
+        <table className="table" id="myTable">
             <thead>
                 <tr>
                 <th scope="col">ID</th>
@@ -34,6 +60,9 @@ function Pocetna({pregledi}) {
                 </tr>
             </thead>
             <tbody>
+ 
+
+
                 {sort === true ? 
                 
                         pregledi
